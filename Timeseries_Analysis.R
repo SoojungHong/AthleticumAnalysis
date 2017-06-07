99bb41bf706499b9e8952a0f3dd0c05354993630
HONG <soojung.hong@atos.net>
2017-06-07 13:10:27 +0200

    adding join with product

--- Timeseries_Analysis.R
+++ Timeseries_Analysis.R
@@ -5,12 +5,14 @@
 # Soojung Hong 
 #---------------------------------------------------
 
-#--------------------
-# library 
+#-------------------------
+# package installation 
 install.packages('plyr')
-
-#plot
 install.packages("ggplot2")
+
+
+#-------------------------
+# library for plot
 library(ggplot2)
 
 
@@ -20,26 +22,68 @@ library(ggplot2)
 trainingData <- 'C:/Users/a613274/Downloads/AthExportData/factSalesTransaction.csv'
 con = file(trainingData, "r")
 
+
 #----------------
 # Read 10000 rows
 partialData <- read.csv(con, nrows=10)
 partialData
 str(partialData)
 
+#-----------------
+# sort by DateID
+sortedData <- partialData[order(partialData$DateID),]
+sortedData
 
-#----------------
-# purchased time 
-timeData <- data.frame(partialData$TimeID, partialData$GrossAmount)
-                    
-timeData
+str(sortedData)
+
+#----------------------------------
+# transaction time & gross amount 
+time_grossamount_Data <- data.frame(sortedData$TimeID, sortedData$GrossAmount)
+time_grossamount_Data
 
 theme_set(theme_bw()) # Change the theme to my preference
-ggplot(aes(x = partialData.TimeID, y = partialData.GrossAmount), data = timeData) + geom_point()
+ggplot(aes(x = sortedData.TimeID, y = sortedData.GrossAmount), data = time_grossamount_Data
+) + geom_point()
 
-#time format conversion 
-timeData <- formatC(partialData$TimeID, width=4, flag="0") #pad 0 if time has 3 digits
+
+#--------------------------------
+# transaction time & warengroup 
+time_product_Data <- data.frame(sortedData$DateID, sortedData$TimeID, sortedData$ProductID)
+time_product_Data
+#need to join with Product data 
+
+#----------------------------------
+#  Read product data 
+productData <- 'C:/Users/a613274/Downloads/AthExportData/dimProduct.csv'
+con2 = file(productData, "r")
+productData <- read.csv(con2, nrows=621542) #All: 621542
+#productData #DO NOT PRINT
+str(productData)
+
+#productData$ProductID
+#productData$UniverseCodeDesc
+#productData$WgrClassCodeDesc
+columns <- c("ProductID", "UniverseCodeDesc", "WgrClassCodeDesc")
+prodDesc <- productData[columns]
+prodDesc
+
+
+#------------------------------------------------------
+# Join 'time_product_Data' and 'productData' by="productID"
+joinedData <- merge(time_product_Data, prodDesc, by="ProductID", all.x = TRUE)
+joinedData
+str(joinedData)
+table(joinedData$WgrClassCodeDesc)
+hist(joinedData$ProductID, col="green", breaks=30, xlab="WarenGrp", main="Histogram of # of WarenGroup") 
+library(plyr)
+count(joinedData$WgrClassCodeDesc)
+
+
+
+#----------------------------
+# Etc
+# 1. time format conversion 
+timeData <- formatC(sortedData$TimeID, width=4, flag="0") #pad 0 if time has 3 digits
 timeData
 format(strptime(timeData, format="%H%M"), format = "%H:%M")
 timeData
-theme_set(theme_bw()) # Change the theme to my preference
-ggplot(aes(x = timeID, y = GrossPrice), data = timeData) + geom_point()
